60d624fdaf6fd95bc0ece9c87d7f334ffde4e382
leonidlisnevskiy <leonidlisnevskiy@gmail.com>
2018-11-02 10:43:11 +0200

    add slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,13 @@
 import sayHello from './lib/sayHello.js';
+import slick from 'slick-carousel';
 
 sayHello();
+
+$('.js-slider').slick({
+	autoplay: true,
+	autoplaySpeed: 4000,
+	dots: false,
+	fade: true,
+	appendArrows: ".js-slider-btns",
+	infinite: true
+});
